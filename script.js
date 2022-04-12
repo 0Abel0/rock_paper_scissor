84797224eecb58d96653532d8cb7d0b217158715
Abel Gideon <abelgideon16@gmail.com>
2022-04-12 13:32:11 +0300

    UI changes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+let roundNumber = 1;
 
 function computerPlay() {
     const choices = ["rock", "paper", "scissor"];
@@ -8,51 +9,52 @@ function computerPlay() {
 }
 
 function playRound(computerSelection, playerSelection) {
-    playerSelection = prompt("Rock Paper Scissor:");
+    playerSelection = prompt(`Round(${roundNumber}/5})\n\nRock Paper Scissor:`);
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerPlay();
 
     if (playerSelection === computerSelection) {
-        alert(`YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\nIt's a tie`);
+        alert(`YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\n\nIt's a tie`);
     }
     else if (computerSelection == "rock" && playerSelection == "scissor") {
         computerScore += 1;
-        alert(`YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\nYou lost ${computerSelection} beats ${playerSelection}`);
+        alert(`YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\n\nYou lost, ${computerSelection} beats ${playerSelection}`);
     }
     else if (computerSelection == "rock" && playerSelection == "paper") {
         playerScore += 1;
-        alert( `YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\nYou won ${playerSelection} beats ${computerSelection}`);
+        alert( `YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\n\nYou won, ${playerSelection} beats ${computerSelection}`);
     }
     else if (computerSelection == "paper" && playerSelection == "scissor") {
         playerScore += 1;
-        alert(`YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\nYou won ${playerSelection} beats ${computerSelection}`);
+        alert(`YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\n\nYou won, ${playerSelection} beats ${computerSelection}`);
     }
     else if (computerSelection == "paper" && playerSelection == "rock") {
         computerScore += 1;
-        alert(`YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\nYou lost ${computerSelection} beats ${playerSelection}`);
+        alert(`YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\n\nYou lost, ${computerSelection} beats ${playerSelection}`);
     }
     else if (computerSelection == "scissor" && playerSelection == "rock") {
         playerScore += 1;
-        alert(`YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\nYou won ${playerSelection} beats ${computerSelection}`);
+        alert(`YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\n\nYou won, ${playerSelection} beats ${computerSelection}`);
     }
     else if (computerSelection == "scissor" && playerSelection == "paper") {
         computerScore += 1;
-        alert(`YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\nYou lost ${computerSelection} beats ${playerSelection}`);
+        alert(`YOU: ${playerSelection}\nCOMPUTER: ${computerSelection}\n\nYou lost, ${computerSelection} beats ${playerSelection}`);
     };
 };
 
 function game() {
     for (let i = 0; i < 5; i++) {
         playRound();
+        roundNumber += 1;
     }
     if (playerScore > computerScore) {
-        alert(`You have defeated the computer\nYOU: ${playerScore}\nCOMPUTER: ${computerScore}`);
+        alert(`You have defeated the computer\n\nYOU: ${playerScore}\nCOMPUTER: ${computerScore}`);
     }
     else if (computerScore > playerScore) {
-        alert(`The computer has gotten the better of you\nYOU: ${playerScore}\nCOMPUTER: ${computerScore}`);
+        alert(`The computer has gotten the better of you\n\nYOU: ${playerScore}\nCOMPUTER: ${computerScore}`);
     }
     else {
-        alert(`You and the computer are an equal match\nYOU: ${playerScore}\nCOMPUTER: ${computerScore}`);
+        alert(`You and the computer are an equal match\n\nYOU: ${playerScore}\nCOMPUTER: ${computerScore}`);
     }
 };
 
